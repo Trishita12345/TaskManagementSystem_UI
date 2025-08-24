10191d9992d319a17b3445d10ef4268828ff8e82
Trishita Majumder <trishita.majumder15@gmail.com>
2025-08-25 01:42:58 +0530

    date validation added

--- src/pages/Tasks/ViewTask/components/Date.tsx
+++ src/pages/Tasks/ViewTask/components/Date.tsx
@@ -1,9 +1,9 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import type { UpdateableTaskComponentProps } from "../../../../constants/types";
 import TaskDetailsLabelValue from "./TaskDetailsLabelValue";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { format, isValid } from "date-fns";
+import { format, isAfter, isBefore, isValid, parseISO } from "date-fns";
 
 const Date = ({
   selectedTask,
@@ -13,33 +13,79 @@ const Date = ({
   const { startDate, endDate } = selectedTask;
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
   const type = dateType === "startDate" ? startDate : endDate;
-  console.log(isEditMode);
-  return (
-    <TaskDetailsLabelValue
-      label={dateType === "startDate" ? "Start Date" : "End Date"}
-      Component={
-        <>
-          {type === null || isEditMode ? (
-            <DatePicker
-              sx={{ ".MuiPickersSectionList-root": { py: 0.5 } }}
-              disablePast
-              onClose={() => setIsEditMode(false)}
-              onChange={(date: any) => {
-                if (isValid(date))
-                  updateTask(
-                    dateType,
-                    format(date as Date, "yyyy-MM-dd"),
-                    undefined,
-                    setIsEditMode
-                  );
-              }}
-            />
-          ) : (
-            <Typography onClick={() => setIsEditMode(true)}>{type}</Typography>
-          )}
-        </>
+  const [errorText, setErrorText] = useState<string>("");
+  const [localDate, setLocalDate] = useState<string>();
+
+  useEffect(() => {
+    setLocalDate(type);
+  }, [selectedTask, dateType]);
+
+  const handleChange = (date: any) => {
+    if (!date || !isValid(date)) return;
+    setLocalDate(format(date as Date, "yyyy-MM-dd"));
+    if (dateType === "startDate") {
+      if (
+        endDate &&
+        isValid(parseISO(endDate)) &&
+        isAfter(date, parseISO(endDate))
+      ) {
+        setErrorText("Start date cannot be after end date");
+        return;
       }
-    />
+    } else if (dateType === "endDate") {
+      if (
+        startDate &&
+        isValid(parseISO(startDate)) &&
+        isBefore(date, parseISO(startDate))
+      ) {
+        setErrorText("End date cannot be before start date");
+        return;
+      }
+    }
+    setErrorText("");
+    updateTask(
+      dateType,
+      format(date as Date, "yyyy-MM-dd"),
+      undefined,
+      setIsEditMode
+    );
+  };
+  return (
+    <>
+      {type === null || isEditMode ? (
+        <Grid container>
+          <TaskDetailsLabelValue
+            label={dateType === "startDate" ? "Start Date" : "End Date"}
+            Component={
+              <DatePicker
+                sx={{ ".MuiPickersSectionList-root": { py: 0.5 } }}
+                disablePast
+                onChange={handleChange}
+                value={
+                  localDate && isValid(parseISO(localDate))
+                    ? parseISO(localDate)
+                    : null
+                }
+                slotProps={{
+                  textField: {
+                    error: Boolean(errorText), // true if error exists
+                    helperText: errorText, // show message under field
+                  },
+                }}
+              />
+            }
+          />
+          {/* <Typography ml={}>{errorText}</Typography> */}
+        </Grid>
+      ) : (
+        <TaskDetailsLabelValue
+          label={dateType === "startDate" ? "Start Date" : "End Date"}
+          Component={
+            <Typography onClick={() => setIsEditMode(true)}>{type}</Typography>
+          }
+        />
+      )}
+    </>
   );
 };
 export default Date;
